8e44a0c8fed6bc2f619d6e404b995c62cc999c2c
digiadsgr-lgtm <digiadsgr@gmail.com>
2026-05-09 11:47:11 +0300

    feat: add vercel analytics and google tag manager

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import {NextIntlClientProvider} from 'next-intl';
 import {getMessages, setRequestLocale} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const playfair = Playfair_Display({ subsets: ["latin"], variable: '--font-serif' });
@@ -80,17 +81,39 @@ export default async function LocaleLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7060949710564119"
           crossOrigin="anonymous"
         ></script>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KH4LD998');
+          `}
+        </Script>
       </head>
       <body
         suppressHydrationWarning
         className={`${inter.variable} ${playfair.variable} antialiased bg-[#030b15] text-[#FAF9F6] overflow-x-hidden`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KH4LD998"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <NextIntlClientProvider messages={messages}>
           <Header />
           <SmoothScroller>
             {children}
           </SmoothScroller>
         </NextIntlClientProvider>
+        <Analytics />
       </body>
     </html>
   );
